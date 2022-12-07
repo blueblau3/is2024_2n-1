ebec4f73f9569bb47e00fdbfcb3876975892587a
Kengo Matsumoto <kengoyam72@gmail.com>
2022-12-07 13:34:06 +0900

    create tables done.

--- obog/obog_db.py
+++ obog/obog_db.py
@@ -8,23 +8,26 @@ def create_obogs_table():
     con = sqlite3.connect(DATABASE)
     #テーブル作成(カラムは8つ。slack参照。)
     con.execute("create table if not exists obogs \
-    (id integer primary key autoincrement, \
-    name text not null, \
-    email text not null, \
-    prefecture text not null, \
-    juniorhighschoolname text not null, \
-    highschoolname text not null, \
-    teaching_area text not null, \
-    pr text not pull \
-    )")
+        (id integer primary key autoincrement, \
+        name text not null, \
+        email text not null, \
+        prefecture text not null, \
+        juniorhighschoolname text not null, \
+        highschoolname text not null, \
+        teaching_area text not null, \
+        pr text not null \
+        )")
     #テーブルにコミット
     con.commit()
 
     #DB起動テスト用
-    con.execute("INSERT INTO obogs \
-    (name, email, prefecture, juniorhighschoolname, highschoolname, teaching_area, pr) \
-    values (?,?,?,?,?,?)",["kengo","aaa@gmail.com","hyogo","shiomi","kobe","badminton,tabletennis","This record is test."]
-    )
-    #テーブルにコミット
-    con.commit()
+    # con.execute("INSERT INTO obogs \
+    # (name, email, prefecture, juniorhighschoolname, highschoolname, teaching_area, pr) \
+    # values (?,?,?,?,?,?,?)",["kengo","aaa@gmail.com","hyogo","shiomi","kobe","badminton,tabletennis","This record is test."]
+    # )
+    # con.commit()
+    #接続解除
     con.close()
+
+#DB起動テスト用
+# create_obogs_table()
